3702d0dd564fa289bbd4efb4c60f2f02fe267d20
Kevin Raja M <kevin@Kevins-MacBook-Pro.local>
2026-02-04 18:24:28 +0530

    alignment changes

--- javascript.js
+++ javascript.js
@@ -1,25 +1,21 @@
-//Day 1 of javascript
-console.log("Vasanth");
-let task1="GetUp Early";
-let task2="Drink 3 litres of Water";
-let task3="Starts Javascript Practice";
-let tasks=[task1,task2,task3];
-let task4="Debugging the code";
-function addTask(task){
+let task1 = "GetUp Early";
+let task2 = "Drink 3 litres of Water";
+let task3 = "Starts Javascript Practice";
+let tasks = [task1, task2, task3];
+let task4 = "Debugging the code";
+function addTask(task) {
     tasks.push(task);
 }
 addTask("read book");
-function tasklist(){
+function tasklist() {
     for (let index = 0; index < tasks.length; index++) {
-    const element = tasks[index];
-    console.log(element);
- }
+        const element = tasks[index];
+        console.log(element);
+    }
 }
 tasklist();
-let task={
-    title:"Eat Lunch",
-    isComplete:true
+let task = {
+    title: "Eat Lunch",
+    isComplete: true
 };
-console.log (task)  
-
-    
+console.log(task)
